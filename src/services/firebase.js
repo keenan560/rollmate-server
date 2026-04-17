3c74f95e75993415f54b609ab091577a1e5d0436
Keenan Mapp <keenanmapp@MacBook-Pro.local>
2026-04-17 17:52:57 -0500

    firebase prod

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,17 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../roll-mate-firebase-adminsdk-dvpro-08ec4a8e36.json");
 
 // Initialize Firebase Admin
+// In production, load service account from FIREBASE_SERVICE_ACCOUNT env var (base64-encoded JSON)
+// In development/UAT, fall back to the local service account file
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString(),
+  );
+} else {
+  serviceAccount = require("../../roll-mate-firebase-adminsdk-dvpro-08ec4a8e36.json");
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
